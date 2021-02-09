3013170508711b45685508dd51e363a858d1c971
GraaafiK <andres_thomas@hotmail.fr>
2021-02-09 15:36:45 +0100

    Added many checks when adding new StackedVars Added the AddStackedVar function to add new vars instead of adding stuff directly through arrays.

--- stackedvar/plugin/sh_hooks.lua
+++ stackedvar/plugin/sh_hooks.lua
@@ -6,16 +6,45 @@ function PLUGIN:ClockworkAddSharedVars(globalVars, playerVars)
 	end;
 end;
 
-function PLUGIN:AddStackedVar(name, default, )
-
-
+function PLUGIN:AddStackedVar(name, default)
+	if type(name) == "string" then
+		if table.HasValue(StackedVarsList,name) then
+			print("Error trying to add a already used value name");
+		else
+			StackedVarsList[#StackedVarsList+1] = name;
+		end;
+	else
+		print("Error trying to add a non string value name");
+	end
+	if Clockwork.config:Get("stackedvars_power") > 2 then
+		if type(default) == "number"  then
+			StackedVarsDefaultValueListPowerCst[#StackedVarsDefaultValueListPowerCst+1] = default;
+		else
+			print("Error trying to add a non number default value");
+		end;
+	else
+		if type(default) == "bool" or type(default) == "number" then
+			StackedVarsDefaultValueListPower2[#StackedVarsDefaultValueListPower2+1] = default;
+		else
+			print("Error trying to add a non bool/number default value");
+		end;
+	end;
 end;
 
+StackedVarsList = {}
+StackedVarsDefaultValueListPower2 = {}
+StackedVarsDefaultValueListPowerCst = {}
+
+PLUGIN:AddStackedVar("Valeur1", true)
+PLUGIN:AddStackedVar("Valeur2", false)
+PLUGIN:AddStackedVar("Valeur3", false)
 
 /////////////////////////////////
--- Clockwork.config:Get("stackedvars_power"):Get()^#StackedVarsList MUST ALWAYS BE INCLUDED BETWEEN 0 AND 2^64
+-- Clockwork.config:Get("stackedvars_power"):Get()^#StackedVarsList MUST ALWAYS BE INCLUDED BETWEEN 0 AND 2^32
 /////////////////////////////////
 
+--Old method.
+--[[
 StackedVarsList = { --Simple value name
 	"Valeur1",
 	"Valeur2",
@@ -38,3 +67,4 @@ StackedVarsDefaultValueListPowerCst = { --Only used if Clockwork.config:Get("sta
 	0,
 	2
 }
+]]
